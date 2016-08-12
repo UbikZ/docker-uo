503ab666fcdd37c9458c97ed1d85ddf6c4b1d035
Gabriel Malet <gabrielmalet@gmail.com>
2016-08-12 20:52:48 +0200

    Add some stuff

--- uo/scripts/entrypoint.sh
+++ uo/scripts/entrypoint.sh
@@ -15,6 +15,10 @@ if [[ ! -f /srv/docker/uo/.runuo ]]; then
 	cp -R runuo-nerun-distro-master/Distro/* runuo-master
 fi
 
+cp -f RunUO.exe.conf runuo-master/
+cp -f Gold.cs runuo-master/Scripts/Items/Misc
+cp -f BankCheck.cs runuo-master/Scripts/Items/Misc
+
 cd runuo-master && \
 dmcs -optimize+ -unsafe -t:exe -out:RunUO.exe -win32icon:Server/runuo.ico -nowarn:219,414 -d:MONO -recurse:Server/*.cs /reference:System.Drawing.dll
 
