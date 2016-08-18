c68c1b4edaa989df570ba21e000a258886cee88f
Gabriel Malet <gabrielmalet@gmail.com>
2016-08-18 16:48:20 +0200

    Improve stuff

--- uo/scripts/entrypoint.sh
+++ uo/scripts/entrypoint.sh
@@ -20,10 +20,12 @@ cp -f RunUO.exe.config runuo-master/
 cp -f Gold.cs runuo-master/Scripts/Items/Misc
 cp -f BankCheck.cs runuo-master/Scripts/Items/Misc
 cp -f DataPath.cs runuo-master/Scripts/Misc
+
 # Add conf stuff within file and replace original after
-sed -i "s/__UO_ADMIN_USERNAME__/$UO_ADMIN_USERNAME/g" AccountPrompt.cs && \
-sed -i "s/__UO_ADMIN_PASSWORD__/$UO_ADMIN_PASSWORD/g" AccountPrompt.cs && \
-cp -f AccountPrompt.cs runuo-master/Scripts/Misc
+sed -i -e "s/__UO_ADMIN_USERNAME__/$UO_ADMIN_USERNAME/g" -e "s/__UO_ADMIN_PASSWORD__/$UO_ADMIN_PASSWORD/g" AccountPrompt.cs &&
+cp -f AccountPrompt.cs runuo-master/Scripts/Misc && \
+sed -i -e "s/__UO_SERVER_NAME__/$UO_SERVER_NAME/g" -e "s/__UO_SERVER_IP__/$UO_SERVER_IP/g" AccountPrompt.cs && \
+cp -f ServerList.cs runuo-master/Scripts/Misc
 
 cd runuo-master && \
 dmcs -optimize+ -unsafe -t:exe -out:RunUO.exe -win32icon:Server/runuo.ico -nowarn:219,414 -d:MONO -recurse:Server/*.cs /reference:System.Drawing.dll
